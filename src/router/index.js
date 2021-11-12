0d13a25d300483e591cb410c94960a11cdd8ef4d
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2021-11-13 03:31:05 +0800

    存個檔

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Index from "../views/Index.vue";
 
 const routes = [
   {
     path: "/",
+    name: "Index",
+    component: Index,
+  },
+  {
+    path: "/home",
     name: "Home",
     component: Home,
   },
---
+++ src/views/Index.vue
@@ -0,0 +1,45 @@
+<script>
+// @ is an alias to /src
+//import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+    name: "Index" /*,
+  components: {
+    HelloWorld,
+  },*/,
+};
+</script>
+
+<template>
+  <div id="index">
+    <h3 id="title">TRANS</h3>
+    <router-link id="entryPoint" to="/Home">Entry</router-link>
+  </div>
+  <!--<HelloWorld msg="Welcome to Your Vue.js App" />-->
+</template>
+
+
+<style lang="scss">
+body{
+  background-color: #D6C7A6;
+}
+#index {
+  margin-top: 30vh;
+  #title{
+    color: white;
+    font-size: 6em;
+    letter-spacing: 1em;
+  }
+  #entryPoint {
+    font-weight: bold;
+    color: white;
+    background-color: #655A65;
+    padding: 1em 3em;
+    border-radius: 3em;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
