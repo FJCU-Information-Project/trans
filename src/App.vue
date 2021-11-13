207b109c706d85a591e07d4bfd0b1f7c6c747ba0
im39user <im39user@outlook.com>
2021-11-13 22:44:58 +0800

    精美的山

--- src/App.vue
+++ src/App.vue
@@ -8,5 +8,8 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  height: 100%;
+  padding: 0;
+  margin: 0;
 }
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,7 +1,6 @@
 <script>
 // @ is an alias to /src
 //import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
     name: "Index" /*,
   components: {
@@ -14,7 +13,13 @@ export default {
   <div id="index">
     <h3 id="title">TRANS</h3>
     <router-link id="entryPoint" to="/Home">Entry</router-link>
+    
   </div>
+  <img id="moutain" src="../assets/svg/moutain.svg" alt="">
+  <!-- <svg id="moutain">
+    <polyline points="135 40, 85 100, 185 100" fill="#a3a87f"/>
+    <polyline points="65 0, 0 100, 130 100" fill="#5b6c5b"/>
+  </svg> -->
   <!--<HelloWorld msg="Welcome to Your Vue.js App" />-->
 </template>
 
@@ -23,19 +28,34 @@ export default {
 body{
   background-color: #D6C7A6;
 }
+#moutain{
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 40vw;
+}
 #index {
   margin-top: 30vh;
   #title{
     color: white;
     font-size: 6em;
     letter-spacing: 1em;
+    text-indent: 1em;
   }
   #entryPoint {
+    letter-spacing: 0.5em;
+    text-indent: 0.5em;
+    text-align: center;
     font-weight: bold;
+    font-size: 1.5em;
     color: white;
+    display: inline-block;
     background-color: #655A65;
-    padding: 1em 3em;
+    box-sizing: border-box;
+    padding: 0.5em 1.5em;
     border-radius: 3em;
+    text-transform: uppercase;
+    text-decoration: none;
 
     &.router-link-exact-active {
       color: #42b983;
