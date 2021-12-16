e749f3f8ff0fe4b124bf0d765d1b640ed4295612
joanne912 <93272852+joannechen912@users.noreply.github.com>
2021-12-16 16:01:47 +0800

    upload css done

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,9 +1,51 @@
 <template>
   <!-- 上傳資料及頁面 -->
   <el-row class="card-allocate">
-    <el-col :span="7"><div class="grid-content bg-purple upload-card"></div></el-col>
-    <el-col :span="7"><div class="grid-content bg-purple-light upload-card"></div></el-col>
-    <el-col :span="7"><div class="grid-content bg-purple upload-card"></div></el-col>
+    <el-col :span="7">
+      <div class="grid-content bg-purple upload-card">
+        <div class="icon-con">
+          <img src="https://cdn-icons.flaticon.com/png/512/4292/premium/4292316.png?token=exp=1639641448~hmac=6280b8cb0beb9320f2d7efbd49eff5c6" alt="">
+        </div>
+        <h3>節點 Node</h3>
+        <p class="describe">
+
+        </p>
+        <form action="">
+          <input type="file" name="" id="">
+          <input type="submit" value="">
+        </form>
+      </div>
+    </el-col>
+    <el-col :span="7">
+      <div class="grid-content bg-purple-light upload-card">
+        <div class="icon-con">
+          <img src="https://cdn-icons-png.flaticon.com/512/6389/6389267.png" alt="">
+        </div>
+        <h3>屬性 Attribute</h3>
+        <p class="describe">
+
+        </p>
+        <form action="">
+          <input type="file" name="" id="">
+          <input type="submit" value="">
+        </form>
+      </div>
+    </el-col>
+    <el-col :span="7">
+      <div class="grid-content bg-purple upload-card">
+        <div class="icon-con">
+          <img src="https://cdn-icons-png.flaticon.com/512/995/995646.png" alt="">
+        </div>
+        <h3>關聯 Relationship</h3>
+        <p class="describe">
+
+        </p>
+        <form action="">
+          <input type="file" name="" id="">
+          <input type="submit" value="">
+        </form>
+      </div>
+    </el-col>
   </el-row>
   <!-- <div id="upload">
     TRANS
@@ -23,7 +65,54 @@ export default {
 <style lang="scss">
   // 上傳資料頁面CSS
   .upload-card{
+    box-sizing: border-box;
     height: 35em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    & h3{
+      margin-top: 2em;
+      font-size: 1.25em;
+      font-weight: bolder;
+      color: #000;
+    }
+    & form{
+      margin-top: 5em;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      & input:nth-child(1){
+        border-radius: 30px;
+        background: #fff;
+        padding: .5em 0;
+      }
+      & input:nth-child(2){
+        margin-top: 3em;
+        width: 30%;
+        border: 0;
+        background-color: #fff;
+        border-radius:10px;
+        padding: .25em;
+        font-size: 1.05em;
+        letter-spacing: 5px;
+        text-indent: 5px;
+        font-weight: bolder;
+      }
+    }
+  }
+  .icon-con{
+    width: 150px;
+    height: 150px;
+    background: #fff;
+    border-radius: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    & img{
+      width: 60%;
+      height: 60%;
+    }
   }
   .card-allocate{
     width: 100%;
