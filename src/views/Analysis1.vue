4d191fb9092c1cccf76ebc65964cfacb631cab96
joanne912 <93272852+joannechen912@users.noreply.github.com>
2021-12-18 19:44:01 +0800

    analysis aside done

--- src/views/Analysis1.vue
+++ src/views/Analysis1.vue
@@ -1,13 +1,23 @@
 <template>
-  <el-select v-model="value" placeholder="Select">
-    <el-option
-      v-for="item in options"
-      :key="item.value"
-      :label="item.label"
-      :value="item.value"
-    >
-    </el-option>
-  </el-select>
+  <el-container>
+    <el-aside width="300px">
+      <el-select v-model="value" placeholder="Select">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
+        </el-option>
+      </el-select>
+    </el-aside>
+    <el-main>
+      <iframe src="" frameborder="0">
+        <!-- 社會網路圖 -->
+      </iframe>
+    </el-main>
+  </el-container>
+
 </template>
 
 <script>
@@ -15,3 +25,44 @@ export default {
   name: "Analysis1",
 };
 </script>
+
+<style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 5em 0;
+  height: 92vh;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
